d61ec1c4ff86f33a94a847386c1979321d94110d
Sarfaraj Bhatti <139355037+sarfarajbhatti@users.noreply.github.com>
2024-04-04 10:31:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-// import CounterApp from "./counter";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
